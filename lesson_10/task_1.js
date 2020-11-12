1e34a092e847c1bd75adb82c43185f8d4ec021c9
shuorin <alreineck@gmail.com>
2020-11-12 14:43:25 +0300

    first task

---
+++ lesson_10/task_1.js
@@ -0,0 +1,11 @@
+let s = 1000;
+let kolichestvoGryadok = Math.floor(s / (15 *25));
+
+
+let osatokGryadok = function (sGryadki) {
+    return sGryadki - (kolichestvoGryadok * 15 * 25);
+}
+
+console.log("Всего грядок: ", kolichestvoGryadok);
+console.log("Остаток участка: ", osatokGryadok(s));
+
