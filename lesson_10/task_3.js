879f562c0ee56c599c8ac849b5ad2c40f88c79f6
shuorin <alreineck@gmail.com>
2020-11-12 15:46:35 +0300

    task 4

--- lesson_10/task_3.js
+++ lesson_10/task_3.js
@@ -1,11 +1,1 @@
-let a = 3;
-let b = 45;
-let c = 13;
-
-if ((a > b) && (a > c)) {
-    console.log(a);
-} else if ((b > a) && (b > c)) {
-    console.log(b);
-} else {
-    console.log(c);
-}
+console.log(Math.min(67, 65, 35));
---
+++ lesson_10/task_4.js
@@ -0,0 +1,9 @@
+let a = 33;
+let b = 10;
+let c = 13;
+
+if (Math.abs(a - b) < Math.abs(c - b)) {
+    console.log(a);
+} else {
+    console.log(c);
+}
