8b7c70302e0f7ea956664ce79f5b25ab9fb8a9a7
shuorin <alreineck@gmail.com>
2020-11-16 14:00:43 +0300

    task_1 (2)

--- lesson_11/task_1.js
+++ lesson_11/task_1.js
@@ -3,14 +3,11 @@ let win = "winter", spr = "spring", sum = "summer", aut = "autumn";
 for (let i = 1; i < 13; i++ ) {
     if ((i <= 2) || (i >= 12)) {
         console.log(i, win);
-    }
-    if ((i > 2) && (i <= 5)) {
+    } else if ((i > 2) && (i <= 5)) {
         console.log(i, spr);
-    }
-    if ((i > 5) && (i <= 8)) {
+    } else if ((i > 5) && (i <= 8)) {
         console.log(i, sum);
-    }
-    if ((i > 8) && (i <= 11)) {
+    } else {
         console.log(i, aut);
     }
 }
