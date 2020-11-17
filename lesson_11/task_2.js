b062dd6a2a853ff57ef4e0f1a5ba5d20eebaecfc
shuorin <alreineck@gmail.com>
2020-11-17 13:02:09 +0300

    tast_2(2)

--- lesson_11/task_2.js
+++ lesson_11/task_2.js
@@ -2,18 +2,18 @@ let l = 2.5
 
 for (let i = 1; i <= 5; i++) {
     if (i = 1) {
-        console.log(l * 10);
+        console.log(l / 10);
     } 
     if (i = 2) {
-        console.log(l / 1000);
+        console.log(l * 1000);
     }
     if (i = 3) {
         console.log(l * 1);
     } 
     if (i = 4) {
-        console.log(l * 1000);
+        console.log(l / 1000);
     } 
     if (i = 5) {
-        console.log(l * 100);
+        console.log(l / 100);
     }
 }
